3d888c2d322af8ee65e9627a71b5d6fede28b630
demitryedwards <dxyz773@gmail.com>
2023-04-13 18:39:20 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const imputField = e.target.new_task_description.value;
+    createTaskEl(imputField);
+    form.reset();
+  });
 });
+
+function createTaskEl(input) {
+  const p = document.createElement("p");
+  p.textContent = `${input}  `;
+  const btn = document.createElement("button");
+  btn.textContent = "x";
+  btn.addEventListener("click", removeTaskEl);
+  const check = document.createElement("input");
+  check.type = "checkbox";
+  p.style.display = "flex";
+  p.style.gap = "2px";
+  p.style.alignItems = "center";
+  p.appendChild(btn);
+  p.appendChild(check);
+  document.getElementById("tasks").appendChild(p);
+}
+
+function removeTaskEl(e) {
+  e.target.parentNode.remove();
+}
